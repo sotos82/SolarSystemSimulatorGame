a3522411cb10e7d74e2154db94194f6a97b20295
sotos82 <mrsotiris2000@yahoo.gr>
2015-06-25 16:21:16 +0200

    Update DebugScript.cs

--- Assets/Scripts/DebugScript.cs
+++ Assets/Scripts/DebugScript.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class DebugScript : MonoBehaviour
 {
-
 	void Start ()
     {
 	
@@ -11,8 +10,8 @@ public class DebugScript : MonoBehaviour
 	
 	void Update ()
     {
-        Debug.DrawLine(Vector3.zero, new Vector3(5000, 0, 0), Color.red);
-        Debug.DrawLine(Vector3.zero, new Vector3(0, 5000, 0), Color.green);
-        Debug.DrawLine(Vector3.zero, new Vector3(0, 0, 5000), Color.blue);
+       	Debug.DrawLine(Vector3.zero, new Vector3(5000, 0, 0), Color.red);
+       	Debug.DrawLine(Vector3.zero, new Vector3(0, 5000, 0), Color.green);
+       	Debug.DrawLine(Vector3.zero, new Vector3(0, 0, 5000), Color.blue);
 	}
 }
